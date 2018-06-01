16ea2308ab2003de17e0706c25af22d17b0d95d1
Chidimma Uba <chidimmajuba@gmail.com>
2018-06-01 13:10:56 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+"# Input-Output-Debugging-Assignment" 
---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+                double num1, num2, num3, num4;
+
+                System.out.print( "First integer? " );
+                num1 = keyboard.nextDouble();
+
+                System.out.print( "Second integer? " );
+                num2 = keyboard.nextInt();
+
+                System.out.print( "Third integer? " );
+                num3 = keyboard.nextDouble();
+
+                num4 = num1+num2+num3;
+  System.out.println("The total is : " + num4);
+        }
+    }
